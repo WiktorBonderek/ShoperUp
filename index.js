85278b33c18ca373d3279a56518b707e2b7a329c
Wiktor Bonderek <wiktor.bonderek@shoper.pl>
2022-05-30 14:00:45 +0200

    CSS and HTML reset, js file add

---
+++ index.js
@@ -0,0 +1,23 @@
+let dayOfTheWeek = 8;
+let message = " ";
+
+if (dayOfTheWeek == 1) {
+ message = "Poniedziałek";
+} else if (dayOfTheWeek == 2) {
+ message = "Wtorek";
+} else if (dayOfTheWeek == 3) {
+ message = "Środa";
+} else if (dayOfTheWeek == 4) {
+ message = "Czwartek";
+} else if (dayOfTheWeek == 5) {
+ message = "Piątek";
+} else if (dayOfTheWeek == 6) {
+ message = "Sobota";
+} else if (dayOfTheWeek == 7) {
+ message = "Niedziela";
+} else {
+ message = "Zła wartość";
+}
+
+
+console.log(message);
